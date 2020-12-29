3578eb5a717f031bdd01994b3f112ad975cf3a89
Fuad9 <alfuad8548@gmail.com>
2020-12-29 17:17:03 +0600

    done and app deployed to netlify

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 import "./App.css";
+
 import Loading from "./components/utilities/Loading";
 import WeatherData from "./components/WeatherData/WeatherData";
 
 function App() {
    const [loading, setLoading] = useState(false);
-   const [search, setSearch] = useState("");
+   const [search, setSearch] = useState("delhi");
    const [weatherData, setWeatherData] = useState({});
 
+   /*setting up Api key in environment variables ============*/
+   const API_KEY = process.env.REACT_APP_API_KEY;
+
+   /* fetching data from openweathermap=========================== */
    useEffect(() => {
       const getData = async () => {
          try {
             setLoading(true);
             const response = await axios.get(
-               "https://api.openweathermap.org/data/2.5/weather?appid=c0a13cdeb3f7cac0507317486f399f72&q=" +
-                  search
+               `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&unit=metrics&q=
+                  ${search}`
             );
             setWeatherData(response.data);
             setLoading(false);
@@ -25,43 +30,26 @@ function App() {
          }
       };
       getData();
-   }, [search]);
-
-   const { name, dt } = weatherData;
-   //  const { sunrise, sunset } = weatherData.sys;
-   const { lat, lon } = weatherData.coord;
-   const { temp, temp_min, temp_max, humidity, pressure } = weatherData.main;
-   const { main, icon } = weatherData.weather[0];
-   const { speed, deg } = weatherData.wind;
+   }, [API_KEY, search]);
 
    return (
       <div className="App">
+         <h1 className="text-uppercase text-center">Fetch the weather data of your city</h1>
+
          <div className="d-flex justify-content-center my-3">
             <input
                className="form-control w-50"
                type="text"
                onBlur={(e) => setSearch(e.target.value)}
-               placeholder="search products"
+               placeholder="type the name of your city"
             />
-            <button className="btn btn-success">Search</button>
+
+            <button className="btn btn-primary">search</button>
          </div>
 
-         {loading ? (
-            <Loading />
-         ) : (
-            <div>
-               <h2>City Name: {name}</h2>
-               <h3>Time: {new Date(dt * 1000).toLocaleTimeString()}</h3>
-               {/* <h3>Time: {new Date(sunrise * 1000).toLocaleTimeString()}</h3>
-               <h3>Time: {new Date(sunset * 1000).toLocaleTimeString()}</h3>
-               <h3>Time: {new Date(sunset * 1000).toLocaleTimeString()}</h3> */}
-               <h3>Latitude: {lat}</h3>
-               <h3>Longitude: {lon}</h3>
-               <h3>Temperature: {((temp - 32) * (5 / 9)).toFixed(2)}</h3>
-               <h3>Minimum Temperature: {temp_min}</h3>
-               <h3>Maximum Temperature: {temp_max}</h3>
-            </div>
-         )}
+         <div className="d-flex justify-content-center my-5">
+            {loading ? <Loading /> : <WeatherData weatherData={weatherData} />}
+         </div>
       </div>
    );
 }
--- src/components/WeatherData/WeatherData.js
+++ src/components/WeatherData/WeatherData.js
@@ -1,30 +1,45 @@
 import React from "react";
 
 const WeatherData = ({ weatherData }) => {
-   const { name, dt } = weatherData;
-   const { country, sunrise, sunset } = weatherData.sys;
-   const { lat, lon } = weatherData.coord;
-   const { temp, temp_min, temp_max, humidity, pressure } = weatherData.main;
-   const { main, icon } = weatherData.weather[0];
-   const { speed, deg } = weatherData.wind;
-
-   console.log(((temp - 32) * (5 / 9)).toFixed(2));
-
    return (
-      <>
+      <div>
          <h2>
-            City Name: {name}, {country}
+            <span>City Name:</span> {weatherData?.name}, {weatherData?.sys?.country}
          </h2>
-         <h3>Time: {new Date(dt * 1000).toLocaleTimeString()}</h3>
-         <h3>Time: {new Date(sunrise * 1000).toLocaleTimeString()}</h3>
-         <h3>Time: {new Date(sunset * 1000).toLocaleTimeString()}</h3>
-         <h3>Time: {new Date(sunset * 1000).toLocaleTimeString()}</h3>
-         <h3>Latitude: {lat}</h3>
-         <h3>Longitude: {lon}</h3>
-         <h3>Temperature: {((temp - 32) * (5 / 9)).toFixed(2)}</h3>
-         <h3>Minimum Temperature: {temp_min}</h3>
-         <h3>Maximum Temperature: {temp_max}</h3>
-      </>
+         <h3>
+            <span>Time:</span> {new Date(weatherData?.dt * 1000).toLocaleTimeString()}
+         </h3>
+         <h3>
+            <span>Sunrise:</span> {new Date(weatherData?.sys?.sunrise * 1000).toLocaleTimeString()}
+         </h3>
+         <h3>
+            <span>Sunset:</span> {new Date(weatherData?.sys?.sunset * 1000).toLocaleTimeString()}
+         </h3>
+         <h3>
+            <span>Latitude:</span> {weatherData?.coord?.lat}
+         </h3>
+         <h3>
+            <span>Longitude:</span> {weatherData?.coord?.lon}
+         </h3>
+         <h3>
+            <span>Temperature:</span> {(weatherData?.main?.temp - 273.15).toFixed(2)}°C
+         </h3>
+         <h3>
+            <span>Minimum Temperature:</span> {(weatherData?.main?.temp_min - 273.15).toFixed(2)}°C
+         </h3>
+         <h3>
+            <span>Maximum Temperature:</span> {(weatherData?.main?.temp_max - 273.15).toFixed(2)}°C
+         </h3>
+         <h3>
+            <span>Humidity:</span> {weatherData?.main?.humidity}%
+         </h3>
+         <h3>
+            <span>Pressure:</span> {(weatherData?.main?.pressure / 101.325).toFixed(2)}atm
+         </h3>
+         <h3>
+            <span>Wind Speed:</span> {weatherData?.wind?.speed}km/h
+         </h3>
+      </div>
    );
 };
 
